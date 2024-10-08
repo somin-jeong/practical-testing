6fa318174a93befc3884183cdf90fca4a8b0c21a
정소민 <yjk70702002@gmail.com>
2024-10-08 22:35:39 +0900

    ProductRepositoryTest @SpringBootTest로 변경

--- src/test/java/com/example/cafekiosk/spring/domain/product/repository/ProductRepositoryTest.java
+++ src/test/java/com/example/cafekiosk/spring/domain/product/repository/ProductRepositoryTest.java
@@ -8,14 +8,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//@SpringBootTest
-@DataJpaTest
+@SpringBootTest
+//@DataJpaTest
 @ActiveProfiles("test")
 class ProductRepositoryTest {
     @Autowired
