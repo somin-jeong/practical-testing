8943398c24bdd81d01beec53f319c8519751be23
정소민 <yjk70702002@gmail.com>
2024-10-11 00:24:00 +0900

    상품 번호 리스트로 재고 조회하는 StockRepositoryTest 성공 코드

--- src/main/java/com/example/cafekiosk/spring/domain/stock/repository/StockRepository.java
+++ src/main/java/com/example/cafekiosk/spring/domain/stock/repository/StockRepository.java
@@ -3,5 +3,8 @@ package com.example.cafekiosk.spring.domain.stock.repository;
 import com.example.cafekiosk.spring.domain.stock.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StockRepository extends JpaRepository<Stock, Long> {
+    List<Stock> findAllByProductNumberIn(List<String> productNumbers);
 }
