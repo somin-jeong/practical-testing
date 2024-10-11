8a1494165183308a01fbd4f681f07211732fd155
정소민 <yjk70702002@gmail.com>
2024-10-11 19:30:44 +0900

    가장 마지막으로 저장한 상품의 상품 번호를 읽어오는 ProductRepositoryTest 성공 코드

--- src/main/java/com/example/cafekiosk/spring/domain/product/repository/ProductRepository.java
+++ src/main/java/com/example/cafekiosk/spring/domain/product/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.example.cafekiosk.spring.domain.product.repository;
 import com.example.cafekiosk.spring.domain.product.Product;
 import com.example.cafekiosk.spring.domain.product.ProductSellingType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -15,4 +16,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllBySellingTypeIn(List<ProductSellingType> sellingTypes);
 
     List<Product> findAllByProductNumberIn(List<String> productNumbers);
+
+    @Query(value = "select p.productNumber from Product p order by p.id desc limit 1")
+    String findLatestProductNumber();
 }
--- src/test/java/com/example/cafekiosk/spring/domain/product/repository/ProductRepositoryTest.java
+++ src/test/java/com/example/cafekiosk/spring/domain/product/repository/ProductRepositoryTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
@@ -103,5 +102,43 @@ class ProductRepositoryTest {
                         Assertions.tuple("001", "아메리카노", ProductSellingType.SEllING),
                         Assertions.tuple("002", "카페라떼", ProductSellingType.HOLD)
                 );
+    }
+
+    @DisplayName("가장 마지막으로 저장한 상품의 상품 번호를 읽어온다.")
+    @Test
+    void findLatestProduct() {
+        // given
+        Product product1 = Product.builder()
+                .type(ProductType.HANDMADE)
+                .sellingType(ProductSellingType.SEllING)
+                .price(4000)
+                .name("아메리카노")
+                .productNumber("001")
+                .build();
+
+        Product product2 = Product.builder()
+                .type(ProductType.HANDMADE)
+                .sellingType(ProductSellingType.HOLD)
+                .price(5000)
+                .name("카페라떼")
+                .productNumber("002")
+                .build();
+
+        String targetProductNumber = "003";
+        Product product3 = Product.builder()
+                .type(ProductType.HANDMADE)
+                .sellingType(ProductSellingType.STOP_SELLING)
+                .price(7000)
+                .name("팥빙수")
+                .productNumber(targetProductNumber)
+                .build();
+
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        // when
+        String productNumber = productRepository.findLatestProductNumber();
+
+        // then
+        assertThat(productNumber).isEqualTo(targetProductNumber);
     }
 }
