6a83996708cdd74ab5a3c614bc2e857ed29a3e8e
정소민 <yjk70702002@gmail.com>
2024-10-11 19:58:46 +0900

    상품이 하나도 없는 경우에는 NULL을 반환하는.ProductRepositoryTest 성공 코드

--- src/test/java/com/example/cafekiosk/spring/domain/product/repository/ProductRepositoryTest.java
+++ src/test/java/com/example/cafekiosk/spring/domain/product/repository/ProductRepositoryTest.java
@@ -141,4 +141,14 @@ class ProductRepositoryTest {
         // then
         assertThat(productNumber).isEqualTo(targetProductNumber);
     }
+
+    @DisplayName("가장 마지막으로 저장한 상품의 상품 번호를 읽어올 때, 상품이 하나도 없는 경우에는 NULL을 반환한다.")
+    @Test
+    void findLatestProductWhenProductIsEmpty() {
+        // when
+        String productNumber = productRepository.findLatestProductNumber();
+
+        // then
+        assertThat(productNumber).isNull();
+    }
 }
