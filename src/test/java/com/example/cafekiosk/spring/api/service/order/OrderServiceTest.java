6ead58e73dbc42e013df3400a30bb8859afb97ae
정소민 <yjk70702002@gmail.com>
2024-10-11 15:56:57 +0900

    재고가 없는 상품으로 주문을 생성하려는 경우 예외가 발생하는 테스트

--- src/test/java/com/example/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/com/example/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -15,16 +15,17 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cglib.core.Local;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@Transactional
 @SpringBootTest
 @ActiveProfiles("test")
 class OrderServiceTest {
@@ -39,14 +40,14 @@ class OrderServiceTest {
     @Autowired
     private StockRepository stockRepository;
 
-    @AfterEach
-    void tearDown() {
-//        orderProductRepository.deleteAll();
-        orderProductRepository.deleteAllInBatch();
-        productRepository.deleteAllInBatch();
-        orderRepository.deleteAllInBatch();
-        stockRepository.deleteAllInBatch();
-    }
+//    @AfterEach
+//    void tearDown() {
+////        orderProductRepository.deleteAll();
+//        orderProductRepository.deleteAllInBatch();
+//        productRepository.deleteAllInBatch();
+//        orderRepository.deleteAllInBatch();
+//        stockRepository.deleteAllInBatch();
+//    }
 
     @DisplayName("상품 번호 리스트를 받아 주문을 생성한다.")
     @Test
@@ -155,6 +156,32 @@ class OrderServiceTest {
                         Tuple.tuple("001", 0),
                         Tuple.tuple("002", 1)
                 );
+    }
+
+    @DisplayName("재고가 없는 상품으로 주문을 생성하려는 경우 예외가 발생한다.")
+    @Test
+    void createOrderWithNoStock() {
+        // given
+        Product product1 = createProduct(ProductType.BOTTLE, "001", 1000);
+        Product product2 = createProduct(ProductType.BAKERY, "002", 3000);
+        Product product3 = createProduct(ProductType.HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        Stock stock1 = Stock.create("001", 2);
+        Stock stock2 = Stock.create("002", 2);
+        stock1.deductQuantity(1);  // TODO: 이렇게 하면 안됨
+        stockRepository.saveAll(List.of(stock1, stock2));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001", "002", "003"))
+                .build();
+
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        // when // then
+        assertThatThrownBy(() -> orderService.createOrder(request, registeredDateTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("재고가 부족한 상품이 있습니다.");
     }
 
     private Product createProduct(ProductType type, String productNumber, int price) {
