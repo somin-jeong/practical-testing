b5c008480bf1db9b82de3d6caea2068a618e1086
정소민 <yjk70702002@gmail.com>
2024-10-12 20:16:47 +0900

    @Transactional(readOnly = true)

--- src/main/java/com/example/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/com/example/cafekiosk/spring/api/service/product/ProductService.java
@@ -8,12 +8,14 @@ import com.example.cafekiosk.spring.domain.product.ProductType;
 import com.example.cafekiosk.spring.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ProductService {
     private final ProductRepository productRepository;
 
@@ -28,6 +30,7 @@ public class ProductService {
     // 동시셩 이슈
     // DB의 ProductNumber라는 필드에 유니크 인덱스 제약조건을 걸고 재시도하는 로직을 추가한다. 누군가 먼저 번호를 선점했다면 재시도
     // 해결: 상품 번호가 증가하는 값이 아니라 UUID 사용 -> 아예 유니크한 값
+    @Transactional
     public ProductResponse createProduct(ProductCreateRequest request) {
         // DB에서 마지막에 저장된 Product의 상품 번호를 읽어와서 +1
         // 009 -> 010
